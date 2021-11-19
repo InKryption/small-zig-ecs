7e342be5c2d3e0ba5e15bec9dfd6cc2d1e841abf
InKryption <59504965+InKryption@users.noreply.github.com>
2021-11-19 14:16:59 +0100

    removed comment

--- build.zig
+++ build.zig
@@ -1,8 +1,6 @@
 const std = @import("std");
 
 pub fn build(b: *std.build.Builder) void {
-    // Standard release options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const mode = b.standardReleaseOptions();
 
     const lib = b.addStaticLibrary("ecs", "src/main.zig");
